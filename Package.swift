59115e82ede6f6f6947bbca32b6fe8d0ba7ba20f
Junda <junda@raccoons.dev>
2025-01-09 13:07:00 +0800

    use jup fork

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["SolanaWrapper"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/LedgerHQ/ios-ble-wrapper", branch: "main"),
+        .package(url: "https://github.com/jup-ag/ios-ble-wrapper", branch: "main"),
         .package(url: "https://github.com/keefertaylor/Base58Swift.git", from: "2.1.0"),
     ],
     targets: [
